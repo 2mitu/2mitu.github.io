0a65bdb0841c91b9339cf001e474ea386e3a42de
FelixLin <37888183+2mitu@users.noreply.github.com>
2025-02-18 14:09:17 +0800

    Update onetwo.js

--- imageshow/scripts/onetwo.js
+++ imageshow/scripts/onetwo.js
@@ -1,34 +1,41 @@
 //键盘操作：
-//按下方向键 左/右，将显示上一张/下一张。
+//按下方向键 左/右，将显示前一张/后一张。
 //数字键1表示选择左图，数字键2表示选择右图
 
 //放大和缩小:
-//按下数字键1/2的情况下，使用方向键上和下进行放大和缩小；
-//或者在按下Z键的情况下，将鼠标指针移动到图片上，使用鼠标的滚轮对该图片进行放大缩小；
+//使用数字键1/2选择，使用方向键上和下进行放大和缩小；
+//或者按下Z键，将鼠标指针移动到图片上，使用滚轮对该图片进行放大缩小；
 
-//移动:按下C键的情况下，
-//按下数字键1/2的情况下，使用方向键上、下、左、右进行相应方向的移动；
+//移动:先按下C键，
+//按下数字键1/2，使用方向键上、下、左、右进行相应方向的移动；
 //或者将鼠标指针移动到图片上某一点，此时按下鼠标左键将会把该点显示在视图中心；
 var showtwo = false;
+var showkeep = true;
+var showdiscard = true;
 var img1_do = false;
 var img2_do = false;
 var pressed_c = false;
 var pressed_z = false;
 var imglist = new Array();
+var imgdel = new Array();
 var curimg = 0;
+var sndimg = 0;
 var maxRatio = 2;
 var imgarea_w = 0;
 var imgarea_h = 0;
 var menu_w = 120;
-var checkbox_h = 165;
+var checkbox_h = 170;
 var URL = window.URL || window.webkitURL
 
 //获取各个元素
+const showtype = document.getElementsByName('interest');
 const btshow = document.querySelector('#showtwo');
 const imgarea1 = document.querySelector('#imgarea1');
 const imgarea2 = document.querySelector('#imgarea2');
 const image1 = document.querySelector('#img1');
 const image2 = document.querySelector('#img2');
+const mark1 = document.querySelector('#del1');
+const mark2 = document.querySelector('#del2');
 const btfolder = document.querySelector('#folder');
 const getfolder = document.querySelector('#selectfolder');
 const btprv = document.querySelector('#previous');
@@ -36,6 +43,8 @@ const btnext = document.querySelector('#next');
 const imginfo = document.querySelector('#imgnum');
 const btactsize = document.querySelector('#originalsize');
 const btfitsize = document.querySelector('#autofit');
+const btexport = document.querySelector('#export');
+const btdelete = document.querySelector('#delete');
 
 //监测浏览器大小改变
 window.addEventListener('resize', browserResize);
@@ -45,6 +54,10 @@ document.addEventListener('keydown', processKeyDown);
 document.addEventListener('keyup', processKeyUp);
 
 //各元素添加事件
+for (let st of showtype) {
+	st.addEventListener('change', showTypeChanged);
+}
+
 btshow.addEventListener('change', showTwoOnClick);
 btfolder.addEventListener('click', toSelectFolder);
 getfolder.addEventListener('change', getImages);
@@ -58,10 +71,26 @@ image1.addEventListener('wheel', zoomImageByMouse);
 image2.addEventListener('wheel', zoomImageByMouse);
 btactsize.addEventListener('click', showImageActualSize);
 btfitsize.addEventListener('click', showImageAutoFit);
+btexport.addEventListener('click', exportDeletedFileInfo);
+btdelete.addEventListener('click', changeSelectedImageDeleteStatus);
 
 function toSelectFolder(evt) {
 	evt.preventDefault();
 	getfolder.click();
+}
+
+function showTypeChanged(evt) {
+	st = evt.target.value;
+	if (st == 'showall') {
+		showkeep = true;
+		showdiscard = true;
+	} else if (st == 'keep') {
+		showkeep = true;
+		showdiscard = false;
+	} else if (st == 'discard') {
+		showkeep = false;
+		showdiscard = true;
+	}
 }
 
 function getImages(evt) {
@@ -70,29 +99,30 @@ function getImages(evt) {
 		if (f.type.indexOf("image")!=-1 ) {
 			if (!getnew) {
 				imglist.length = 0;
+				imgdel.length = 0;
 				curimg = 0;
 				getnew = true;
 			}
 		//	let imgsrc = URL.createObjectURL(f);
 		//	imglist.push(imgsrc);
 			imglist.push(f);
+			imgdel.push(false);
 		}
 	}
 	if (getnew) {
 		showImageAutoFit();
 	}
-	console.log('Image number:', imglist.length)
+	//console.log('Image number:', imglist.length)
 }
 
 function showImageAutoFit() {
 	if (imglist.length > 0) {
 		displayImageAutoFitSize(image1, curimg);
+		showDelMark(mark1, curimg);
 		if (showtwo) {
-			let sndimg = curimg + 1;
-			if (sndimg >= imglist.length) {
-				sndimg = 0;
-			}	
+			sndimg = getImageIndex(curimg, 1);
 			displayImageAutoFitSize(image2, sndimg);
+			showDelMark(mark2, sndimg);
 		}
 		imginfo.innerHTML = `${curimg + 1} / ${imglist.length}`;
 	}
@@ -100,22 +130,37 @@ function showImageAutoFit() {
 
 function showPreviousImage() {
 	if (imglist.length > 1) {
-		curimg -= 1;
-		if (curimg < 0) {
-			curimg = imglist.length - 1;
-		}
+		curimg = getImageIndex(curimg, -1);
 		showImageAutoFit();
 	}
 }
 
 function showNextImage() {
 	if (imglist.length > 1) {
-		curimg += 1;
-		if (curimg >= imglist.length) {
-			curimg = 0;
-		}
+		curimg = getImageIndex(curimg, 1);
 		showImageAutoFit();
 	}
+}
+
+function getImageIndex(cur, delta) {
+	let pos = cur;
+	let findnext = true;
+	do {
+		pos += delta;
+		if (delta > 0 && pos >= imglist.length) {
+			pos = 0;
+		} else if (delta < 0 && pos < 0) {
+			pos = imglist.length - 1;
+		}
+		let forkeep = !imgdel[pos] && showkeep;
+		let fordiscard = imgdel[pos] && showdiscard;
+		if ( forkeep || fordiscard) {
+			findnext = false;
+		} else if (pos == cur) {
+			findnext = false;
+		}
+	} while (findnext);
+	return pos;
 }
 
 function imageLoaded(evt) {
@@ -151,11 +196,10 @@ function displayImageAutoFitSize(image, imgnum) {
 	image.src = URL.createObjectURL(imgf);
 	image.style.left = 'auto';
 	image.style.top = 'auto';
-    image.title = imgf.name + "\nFile size: " + imgf.size + " bytes";
+    image.title = imgf.webkitRelativePath + "\nFile size: " + imgf.size + " bytes";
 }
 
 function showImageByRatio(image, ratio) {
-//	console.log("Zoome:", image.ratio, "-->",  ratio);
 	let ctn = image.parentElement;
 	ctn.style['justify-content'] = 'flex-start';
 	ctn.style['align-items'] = 'flex-start';
@@ -221,7 +265,7 @@ function processKeyDown(evt) {
         img1_do = true;
 	} else if (evt.keyCode === 50) {
         img2_do = true;
-	}
+	} 
 }
 
 function processKeyUp(evt) {
@@ -258,7 +302,10 @@ function processKeyUp(evt) {
         } else {
 		zoomImageByKey(false);
 	    } 
-    }
+    } else if (evt.keyCode === 68) {
+        //标记/取消 删除
+		changeSelectedImageDeleteStatus(); 
+	}
 }
 
 function changeCenterByKey(drt) {
@@ -268,6 +315,25 @@ function changeCenterByKey(drt) {
         	changeImageCenter(image2, drt);
     	}
 }
+
+function changeSelectedImageDeleteStatus() {
+	if (img1_do) {
+		imgdel[curimg] = !imgdel[curimg];
+		showDelMark(mark1, curimg);
+    } else if (showtwo && img2_do) {
+		imgdel[sndimg] = !imgdel[sndimg];
+		showDelMark(mark2, sndimg);
+    }
+}
+
+function showDelMark(delmark, imgindex) {
+	if (imgdel[imgindex]) {
+		delmark.style['display']= 'flex';
+	} else {
+		delmark.style['display']= 'none';
+	}	
+}
+
 
 function changeImageCenter(img, drt) {
     let ncx = img.cx;
@@ -350,9 +416,9 @@ function moveImage(img, x, y, cx, cy) {
 function zoomImageByMouse(evt) {
 	if ( pressed_z ) {
 		if (evt.deltaY > 0) {
-		    zoomImage(evt.target, true);
-		} else {
 		    zoomImage(evt.target, false);
+		} else {
+		    zoomImage(evt.target, true);
 		}
     }
 }
@@ -361,7 +427,7 @@ function zoomImage(img, tobig) {
 	if ( img.ratio >= img.rmin && img.ratio <= maxRatio ) {
 		let toratio = img.ratio;
 		if (tobig) {
-		    toratio += 0.1;
+		    toratio += 0.11;
 		} else {
 		    toratio -= 0.09;
 		}
@@ -379,16 +445,54 @@ function zoomImage(img, tobig) {
 	    }
     }
 }
+
+function exportDeletedFileInfo(evt) {
+	let OS_win = false;
+	if (navigator.platform.indexOf("Win") > -1) {
+		OS_win = true;
+	}
+	let nextline = '\n';
+	let del_cmd = 'rm "';
+	let delfn = "delete.sh";
+	let delinfo = "#Following files will be deleted";
+	if (OS_win) {
+		nextline = '\r\n';
+		del_cmd = 'del "';
+		delfn = "delete.cmd";
+		delinfo = "REM Following files will be deleted" + nextline + "chcp 65001";
+	}
+	
+	let dn = 0;
+	for (var i = 0; i < imglist.length; i++) {
+		if (imgdel[i]) {
+			let fn = imglist[i].webkitRelativePath;
+			if (OS_win) {
+					fn = fn.replaceAll('/', '\\');
+			}			
+			delinfo += nextline + del_cmd + fn + '"';
+			dn += 1;
+		}
+	}
+	if (dn > 0) {
+		saveTextAsFile(delinfo, delfn);
+	}
+}
+
+
+function saveTextAsFile(text, fileName) {
+     const blob = new Blob([text], { type: 'text/plain' }); 
+     const link = document.createElement('a'); 
+     link.href = URL.createObjectURL(blob); 
+     link.download = fileName; 
+     link.click(); 
+ }
 	
 window.onload = function() {
 //	设置body和菜单的grid布局
 	document.body.style['grid-template-colums'] =  '1fr ' + menu_w + 'px';
-	document.querySelector('#div2').style['grid-template-rows'] = checkbox_h + 'px repeat(8, 1fr)';
+	document.querySelector('#div2').style['grid-template-rows'] = checkbox_h + 'px repeat(7, 1fr)';
 //	初始化显示选项
-	var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-	checkboxes.forEach(function(checkbox) {
-	checkbox.checked = true;
-	});
+	showtype[0].checked = true;
 	btshow.checked = showtwo;
 };
 
